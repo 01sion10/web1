65e63a83b73bb15327d3a0cdff10303675e4962b
01sion10 <90226910+01sion10@users.noreply.github.com>
2021-11-09 11:11:45 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-  $(".nav>li").mouseenter(function() {
-    $(this).find(".sub").stop().slideDown();
-  });
-  $(".nav>li").mouseleave(function(){
-    $(this).find(".sub").stop().slideUp();
-  });
+    $(".nav>li").mouseenter(function() {
+        $(this).find(".sub").stop().slideDown();
+    });
+
+    $(".nav>li").mouseleave(function() {
+        $(this).find(".sub").stop().slideUp();
+    });
+    
+    
 });
